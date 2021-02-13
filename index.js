b1cd9517d62cb1024701c87e66e364c68e6f822f
Evan <evanfujita@gmail.com>
2021-02-13 09:54:23 -0600

    commit tree 7f90653123380c938a338091446fdc86c37e165c

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+
+
+const saturdayFun = (parameter = 'roller-skate') => {
+    return "This Saturday, I want to " + parameter + "!"
+}
+
+const mondayWork = (parameter = 'go to the office') => { 
+    return `This Monday, I will ${parameter}.`
+}
+
+function wrapAdjective (flair = '*'){
+    
+    function inner (param = 'special'){
+        return `You are ${flair}${param}${flair}!`
+    }
+    return inner
+}
+
+const Calculator = {
+    add: function (num1, num2){return (num1 + num2)},
+    subtract: function (num1, num2){return (num1 - num2)},
+    multiply: function (num1, num2){return (num1 * num2)},
+    divide: function (num1, num2){return (num1 / num2)}
+}
+
+function actionApplyer(int, array) {
+    if (array.length === 0){
+        return int
+    } else {
+        return ((int * array[0])+(array[1]))%(array[2])
+    }
+}
